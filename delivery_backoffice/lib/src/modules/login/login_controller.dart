e7f558a84cee57a057759b0d53c2dea567fb16ca
Matheus Hora <matheus11hora@gmail.com>
2023-05-13 14:33:42 -0300

    Novos

--- delivery_backoffice/lib/src/modules/login/login_controller.dart
+++ delivery_backoffice/lib/src/modules/login/login_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:mobx/mobx.dart';
 
+import '../../core/exceptions/unauthorized_exceptions.dart';
 import '../../services/auth/login_service.dart';
 part 'login_controller.g.dart';
 
@@ -16,11 +19,26 @@ abstract class LoginControllerBase with Store {
   final LoginService _loginService;
 
   @readonly
-  LoginStateStatus _status = LoginStateStatus.initial;
+  LoginStateStatus _loginStatus = LoginStateStatus.initial;
 
   LoginControllerBase(this._loginService);
 
-  Future<void> login(String email, String pasword){
-    
+  @readonly
+  String? _errorMessage;
+
+  @action
+  Future<void> login(String email, String pasword) async {
+    try {
+      _loginStatus = LoginStateStatus.loading;
+      await _loginService.execute(email, pasword);
+      _loginStatus = LoginStateStatus.success;
+    } on UnauthorizedExceptions catch (e) {
+      _errorMessage = 'Login ou senha inválido';
+      _loginStatus = LoginStateStatus.error;
+    } catch (e) {
+      log('Error ao realizar login', error: e);
+      _errorMessage = 'Tente novamente mais tarde';
+      _loginStatus = LoginStateStatus.error;
+    }
   }
 }
