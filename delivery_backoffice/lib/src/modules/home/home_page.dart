205721fe7b776625f5f1e52bcf396378a5058678
Matheus Hora <matheus11hora@gmail.com>
2023-05-13 15:08:55 -0300

    Começando o meun

--- delivery_backoffice/lib/src/modules/home/home_page.dart
+++ delivery_backoffice/lib/src/modules/home/home_page.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../base/base_loyout.dart';
+
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: BaseLoyout(
+        body: Container(),
+      ),
+    );
   }
 }
--- delivery_backoffice/lib/src/modules/login/login_page.dart
+++ delivery_backoffice/lib/src/modules/login/login_page.dart
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> with Loader, Messages {
           break;
         case LoginStateStatus.success:
           hideLoader();
-          Modular.to.navigate('/');
+          Modular.to.navigate('/home');
           break;
         case LoginStateStatus.error:
           hideLoader();
